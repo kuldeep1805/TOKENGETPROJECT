0a6c7d1f0f93382d8bef45ea414a763ddbc9a26b
kuldeep1805 <54759218+kuldeep1805@users.noreply.github.com>
2021-07-28 15:11:59 +0530

    Create kds.java

---
+++ TOKENGETPROJECT/kds.java
@@ -0,0 +1,11 @@
+package com.lti.getdemo;
+
+public class UserApp {
+public static void main(String[] args) {
+	System.out.println("Hello world");
+	System.out.println("second hello");
+  
+	System.out.println("third hello");
+    
+}
+}
